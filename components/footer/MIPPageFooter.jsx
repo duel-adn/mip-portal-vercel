1f0bdb804a4255bc8ee28b6cb91f68e97bd665d1
Antonio Di Noto <a.dinoto@dueltv.com>
2021-09-26 17:51:33 +0200

    Versione inseribile in MIPPage

--- components/footer/MIPPageFooter.jsx
+++ components/footer/MIPPageFooter.jsx
@@ -21,37 +21,40 @@ import styles from './MIPPageFooter.module.scss';
  * @returns 
  */
 export default function MIPPageFooter(props) {
+    const className = `${props.className || ''} mip-bg-blue`
     return (
-        <div className={`${props.className} ${styles.page_footer}`}>
-            <nav className={styles.navbar}>
-                <ul>
-                    <li>
-                        <Link href="#"><a>Mappa del sito</a></Link><span>|</span>
-                    </li>
-                    <li>
-                        <Link href="#"><a>Disclaimer</a></Link><span>|</span>
-                    </li>
-                    <li>
-                        <Link href="#"><a>Cookies</a></Link><span>|</span>
-                    </li>
-                    <li>
-                        <Link href="#"><a>Radio</a></Link><span>|</span>
-                    </li>
-                    <li>
-                        <Link href="#"><a>Contatti</a></Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className={styles.credits}>
-                <div className={styles.credit}>
-                    <p>Un servizio di</p>
-                    <img className={styles.piemonte} src="/images/logo-piemonte.svg" alt="regione piemonte" />
-                </div>
-                <div className={styles.credit}>
-                    <p>In collaborazione con</p>
-                    <img className={styles.l5t} src="/images/logo-5t.svg" alt="regione piemonte" />
+        <footer className={className} >
+            <div className={`${styles.page_footer} mip-page-section`}>
+                <nav className={styles.navbar}>
+                    <ul>
+                        <li>
+                            <Link href="#"><a>Mappa del sito</a></Link><span>|</span>
+                        </li>
+                        <li>
+                            <Link href="#"><a>Disclaimer</a></Link><span>|</span>
+                        </li>
+                        <li>
+                            <Link href="#"><a>Cookies</a></Link><span>|</span>
+                        </li>
+                        <li>
+                            <Link href="#"><a>Radio</a></Link><span>|</span>
+                        </li>
+                        <li>
+                            <Link href="#"><a>Contatti</a></Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles.credits}>
+                    <div className={styles.credit}>
+                        <p>Un servizio di</p>
+                        <img className={styles.piemonte} src="/images/logo-piemonte.svg" alt="regione piemonte" />
+                    </div>
+                    <div className={styles.credit}>
+                        <p>In collaborazione con</p>
+                        <img className={styles.l5t} src="/images/logo-5t.svg" alt="regione piemonte" />
+                    </div>
                 </div>
             </div>
-        </div>
+        </footer>
     )
 }
