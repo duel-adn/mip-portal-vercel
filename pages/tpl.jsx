10b9b67f7906e42a31e04cbf59f6bb73c26a2bd5
Antonio Di Noto <a.dinoto@dueltv.com>
2021-09-27 13:43:33 +0200

    Pagina tpl responsive

--- pages/tpl.jsx
+++ pages/tpl.jsx
@@ -1,7 +1,7 @@
 /**
     (C) Duel srl 2021.
 
-    Pagina delle news.
+    Pagina delle notizie del trasporto pubblico locale.
 
     Revision history
 
@@ -10,45 +10,31 @@
     | 2021/08/10 | Duel   | Prima versione                      |
 */
 
-import MIPPageHead from '../components/page/MIPPageHead'
-import MIPPageHeader from '../components/header/MIPPageHeader'
-import MIPPageFooter from '../components/footer/MIPPageFooter'
-import MIPWeatherPanel, {fetchWeatherData} from '../components/weather/MIPWeatherPanel'
+import MIPPage from '../components/page/MIPPage'
 import MIPTPLServiceList from '../components/tpl/MIPTPLServiceList'
 
-export default function Home(props) {
+export default function Tpl(props) {
   return (
-    <>
-      <MIPPageHead title="Trasporto pubblico" />
-      <MIPPageHeader className="mip-page-header" 
-        title="Trasporto pubblico" 
-        titleClassName="mip-bg-blue"
-        breadcrumb='Indietro'/>
-      <main className="mip-bg-light mip-page-main">
-        <MIPTPLServiceList className="mip-page-row"/>
-      </main>
-      <div className="mip-mt-auto">
-        <aside className="mip-page-section mip-weather-panel">
-          <MIPWeatherPanel className="mip-page-row" weatherData={props.weatherData}/>
-        </aside>
-        <footer className="mip-page-footer mip-bg-blue">
-          <MIPPageFooter className="mip-page-row" />
-        </footer>
-      </div>
-    </>
+    <MIPPage
+      title="Trasporto pubblico" 
+      titleClassName="mip-bg-blue"
+      breadcrumb='Indietro'>
+        <MIPTPLServiceList />
+    </MIPPage>
   )
 }
 
 export async function getStaticProps(context) {
-  const eventData = null //await fetchTrafficEventData(context)
-  const weatherData = await fetchWeatherData(context)
-  const publicTransportData = null //await fetchPublicTransportData(context)
+  // const eventData = null //await fetchTrafficEventData(context)
+  // const weatherData = await fetchWeatherData(context)
+  // const publicTransportData = null //await fetchPublicTransportData(context)
   
-  return {
-    props: {
-      eventData,
-      weatherData,
-      publicTransportData
-    },
-  }
+  // return {
+  //   props: {
+  //     eventData,
+  //     weatherData,
+  //     publicTransportData
+  //   },
+  // }
+  return {props: {}}
 }
