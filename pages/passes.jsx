6dc3151245e74c3c76768da5777c054075cbd26c
Antonio Di Noto <a.dinoto@dueltv.com>
2021-09-28 19:44:42 +0200

    Aggiunta pagina passi

---
+++ pages/passes.jsx
@@ -0,0 +1,65 @@
+/**
+    Duel S.p.A.
+
+    Pagina traffico in tempo reale
+
+    Revision history
+
+    | Data       | Autore | Descrizione 
+    | ---------- | ------ | ----------------------------------- |
+    | 2021/08/10 | Duel   | Prima versione                      |
+*/
+
+import MIPPage from '../components/page/MIPPage';
+import MIPPassDataPanel from '../components/pass/MIPPassDataPanel'
+import MIPPassMapPanel from '../components/pass/MIPPassMapPanel';
+import MIPLegend from '../components/forms/MIPLegend';
+import { fetchPassData } from '../components/pass/MIPPassDataList';
+
+const items = [
+  {
+      title: 'Colle chiuso',
+      icon: 'pass-closed'
+  },
+  {
+      title: 'Colle con chisura programmata',
+      icon: 'pass-future-close'
+  },
+  {
+      title: 'Colle aperto',
+      icon: 'pass-open'
+  },
+]
+
+export default function Passes({passData}) {
+  return (
+    <MIPPage className="mip-traffic-page"
+      pageTitle="Colli alpini"
+      title="Colli alpini"
+        titleClassName="mip-bg-accent"
+        breadcrumb='Indietro'>
+          <MIPPassDataPanel 
+            headerClassName="mip-tl-rounded-corners mip-bg-accent"
+            contentClassName="mip-bl-rounded-corners mip-bg-light"
+            title="Colli alpini"
+            titleIcon="traffic"
+            subtitle="Scopri informazioni su colli e trafori"
+            enableSearch={true}
+            searchPlaceholder="Seleziona un colle" 
+            passData={passData}/>
+      <MIPPassMapPanel className="map-panel" passData={passData}/>
+      <MIPLegend className="legend-panel mip-rounded-corners" items={items}/>
+    </MIPPage>
+  )
+}
+
+export async function getStaticProps(context) {
+  const passData = await fetchPassData(context)
+
+  return {
+      props: {
+        passData
+    }
+  }
+}
+  
