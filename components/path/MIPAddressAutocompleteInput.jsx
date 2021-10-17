7e771442a1b0d059c704cee7defcb702d075b68c
Antonio Di Noto <a.dinoto@dueltv.com>
2021-10-17 10:49:18 +0200

    Separato componente per l'input indirizzi

---
+++ components/path/MIPAddressAutocompleteInput.jsx
@@ -0,0 +1,122 @@
+/**
+    Duel srl
+
+    Controllo per l'input di indirizzi per il sistema di calcolo
+    del percorso con funzioni di ricerca e completamento automatico.
+
+    Revision history
+
+    | Data       | Autore | Descrizione 
+    | ---------- | ------ | ----------------------------------- |
+    | 2021/10/14 | Duel   | Prima versione                      |
+*/
+
+import { useState } from 'react';
+import styles from './MIPPath.module.scss'
+import AsyncSelect from 'react-select/async';
+import { mipPathAutocomplete } from './MIPPathAPI';
+
+/**
+ * Styling per il box di input e il dropdown
+ */
+const customStyles = {
+    option: (provided, state) => ({
+        ...provided,
+        color: '#222',
+        //backgroundColor: state.isFocused ? '#368CF7' : 'white',
+    }),
+    container: (provided) => ({
+        ...provided,
+        width: '100%'
+    }),
+    control: (provided) => ({
+        // none of react-select's styles are passed to <Control />
+        ...provided,
+        backgroundColor: 'transparent',
+        color: 'white',
+        border: 'none',
+        borderBottom: '1px solid white',
+        borderRadius: 0,
+    }),
+    placeholder: (provided) => ({
+        ...provided,
+        color: 'white',
+        fontWeight: 300
+    }),
+    input: (provided) => ({
+        ...provided,
+        color: 'white',
+    }),
+    clearIndicator: (provided) => ({
+        ...provided,
+        color: 'lightgray',
+        ':hover': {
+            color: 'white'
+        }
+    }),
+    dropdownIndicator: (provided) => ({
+        ...provided,
+        color: 'lightgray',
+        ':hover': {
+            color: 'white'
+        }
+    }),
+    indicatorSeparator: (provided) => ({
+        ...provided,
+        backgroundColor: 'transparent'
+    }),
+    singleValue: (provided, state) => {
+        const opacity = state.isDisabled ? 0.5 : 1;
+        const transition = 'opacity 300ms';
+
+        return { ...provided, color: 'white', opacity, transition };
+    }
+}
+
+/**
+ * Controllo per la scelta di località per il calcolo del percorso
+ * con funzioni di ricerca e di completamento automatico.
+ * 
+ * @param {string} id id del div esterno
+ * @param {string} placeholder testo del placeholder dell'input
+ * @param {string} icon url dell'icona a sinistra del controllo
+ * @param {string} onChange funzione da chiamare quando l'utente seleziona un'entry dal dropdown
+ * 
+ * @returns il componente pronto per l'uso
+ */
+export default function MIPAddressAutocompleteInput({ id, placeholder, icon, onChange }) {
+    const [searchString, setSearchString] = useState("")
+
+    const handleInputChange = (value) => setSearchString(value)
+    const handleSelect = (location) => {
+        if (onChange) {
+            onChange(id, location)
+        }
+    }
+
+    const loadOptions = async (inputValue, callback) => {
+        const rawData = await mipPathAutocomplete('it', inputValue)
+        if (rawData instanceof Error) {
+            // TODO: show error on control
+            console.log('error' + rawData)
+            return
+        }
+        callback(rawData)
+    }
+    return (
+        <div id={id} className={styles.input}>
+            <img src={icon} aria-hidden="true" alt="icona" />
+            <AsyncSelect
+                styles={customStyles}
+                placeholder={placeholder}
+                loadOptions={loadOptions}
+                defaultOptions
+                isClearable
+                onInputChange={handleInputChange}
+                onChange={(handleSelect)}
+                noOptionsMessage={() => searchString.length < 3 ? "Digitare un indirizzo" : "Località non trovata"}
+                menuShouldScrollIntoView={true}
+            />
+        </div>
+    )
+}
--- components/path/MIPPathDataForm.jsx
+++ components/path/MIPPathDataForm.jsx
@@ -10,148 +10,11 @@
     | 2021/08/10 | Duel   | Prima versione                      |
 */
 
-import styles from './MIPPath.module.scss';
-import { useMemo, useState } from 'react'
-import { debounce } from '../../lib/MIPUtility'
-import MIPRoundedCheckbox from '../forms/MIPRoundedCheckbox';
-import AsyncSelect from 'react-select/async';
-import { mipPathAutocomplete, mipPathSearch } from './MIPPathAPI';
-
-const url = "https://map.muoversinpiemonte.it/autocomplete?lang=it&text="
-async function autocomplete(input) {
-    if (input.length > 3) {
-        const completeUrl = url + input
-        const rawData = await fetch(completeUrl)
-        const data = await rawData.json()
-        return data.features.map(feature => {
-            return {
-                input: input,
-                id: feature.properties.id,
-                name: feature.properties.label,
-                coordinates: feature.geometry.coordinates
-            }
-        })
-    }
-    return []
-}
-
-function InputWithIcon({ id, placeholder, icon }) {
-    const [value, setValue] = useState({})
-    const [items, setItems] = useState([])
-    async function onChange(e) {
-        const items = await autocomplete(e.target.value)
-        setItems(items)
-        setValue(e.target.value)
-    }
-    const deboucedOnChange = useMemo(() =>
-        debounce(onChange, 500), [])
-    return (
-        <div className={styles.input}>
-            <img src={icon} />
-            <input type="text" list={id}
-                placeholder={placeholder} onChange={deboucedOnChange} />
-            <datalist id={id} onChange={(e) => console.log(e)}>
-                {items && items.map((item, index) =>
-                    <option key={item.id} onSelect={() => console.log(item)}>{item.name}</option>
-                )}
-            </datalist>
-
-        </div>
-    )
-}
-
-const customStyles = {
-    option: (provided, state) => ({
-        ...provided,
-        color: '#222',
-        //backgroundColor: state.isFocused ? '#368CF7' : 'white',
-    }),
-    container: (provided) => ({
-        ...provided,
-        width: '100%'
-    }),
-    control: (provided) => ({
-        // none of react-select's styles are passed to <Control />
-        ...provided,
-        backgroundColor: 'transparent',
-        color: 'white',
-        border: 'none',
-        borderBottom: '1px solid white',
-        borderRadius: 0,
-    }),
-    placeholder: (provided) => ({
-        ...provided,
-        color: 'white',
-        fontWeight: 300
-    }),
-    input: (provided) => ({
-        ...provided,
-        color: 'white',
-    }),
-    clearIndicator: (provided) => ({
-        ...provided,
-        color: 'lightgray',
-        ':hover': {
-            color: 'white'
-        }
-    }),
-    dropdownIndicator: (provided) => ({
-        ...provided,
-        color: 'lightgray',
-        ':hover': {
-            color: 'white'
-        }
-    }),
-    indicatorSeparator: (provided) => ({
-        ...provided,
-        backgroundColor: 'transparent'
-    }),
-    singleValue: (provided, state) => {
-        const opacity = state.isDisabled ? 0.5 : 1;
-        const transition = 'opacity 300ms';
-
-        return { ...provided, color: 'white', opacity, transition };
-    }
-}
-
-function AutocompleteAddressInput({id, placeholder, icon, onChange}) {
-    const [searchString, setSearchString] = useState("")
-    const [location, setLocation] = useState(null)
-
-    const handleInputChange = (value) => setSearchString(value)
-    const handleSelect = (location) => {
-        if (onChange) {
-            onChange(id, location)
-        }
-        setLocation(location)
-        console.log(location)
-    }
-
-    const loadOptions = async (inputValue, callback) => {
-        const rawData = await mipPathAutocomplete('it', inputValue)
-        if (rawData instanceof Error) {
-            console.log('error' + rawData)
-            return
-        }
-        callback(rawData)
-    }
-    return  (
-        <div id={id} className={styles.input}>
-        <img src={icon} />
-        <AsyncSelect 
-            styles={customStyles}
-            placeholder={placeholder}
-            loadOptions={loadOptions}
-            defaultOptions
-            isClearable
-            onInputChange={handleInputChange}
-            onChange={handleSelect}
-            noOptionsMessage={() => searchString.length < 3 ? "Digitare un indirizzo" : "Località non trovata"}
-            menuShouldScrollIntoView={true}
-        />
-    </div>
-    )
-}
+import { useState } from 'react';
+import styles from './MIPPath.module.scss'
+import MIPRoundedCheckbox from '../forms/MIPRoundedCheckbox'
+import MIPAddressAutocompleteInput from './MIPAddressAutocompleteInput'
+import { mipPathSearch } from './MIPPathAPI'
 
 const PATH_ORIGIN_ID = 'path_origin'
 const PATH_DEST_ID = 'path_dest'
@@ -221,12 +84,12 @@ export default function MIPPathDataForm(props) {
     return (
         <form className={`${props.className} ${styles.path_data_dialog}`}>
             <div className={styles.path_input}>
-                <AutocompleteAddressInput id={PATH_ORIGIN_ID} icon='/icons/path-start.svg' placeholder='Punto di partenza' 
+                <MIPAddressAutocompleteInput id={PATH_ORIGIN_ID} icon='/icons/path-start.svg' placeholder='Punto di partenza' 
                     onChange={onChangeLocation} />
                 <div className={styles.divisor}>
                     <img src="/icons/path-swap.svg" id='destination' alt="inverti partenza e destinazione" />
                 </div>
-                <AutocompleteAddressInput id={PATH_DEST_ID} icon='/icons/path-dest.svg' placeholder='Punto di arrivo' 
+                <MIPAddressAutocompleteInput id={PATH_DEST_ID} icon='/icons/path-dest.svg' placeholder='Punto di arrivo' 
                     onChange={onChangeLocation} />
             </div>
             <div className={styles.button_bar_title}>Mostra percorso</div>
