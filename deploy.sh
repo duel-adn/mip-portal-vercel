a0998c42fe0b344b1f7aca1176178a498cd61822
antonio di noto <a.dinoto@dueltv.com>
2021-10-18 18:25:42 +0200

    Elimminati script obsoleti

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-PROJECT=mip-mato
-NAME=mip-mockup-image
-TAG=europe-west6-docker.pkg.dev/$PROJECT/mip-portal-mockup/$NAME
-gcloud run deploy $NAME \
---image=$TAG \
---platform=managed \
---region=europe-west6 \
---project=$PROJECT
