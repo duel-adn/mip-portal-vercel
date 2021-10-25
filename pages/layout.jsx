f21c6a04ab3446501ded1bf92beb1b689fe54ea2
dinotoa <sparetimebits@users.noreply.github.com>
2021-10-25 21:32:52 +0200

    Aggiunta icona ambulanza

--- pages/layout.jsx
+++ pages/layout.jsx
@@ -9,6 +9,7 @@
     | ---------- | ------ | ----------------------------------- |
     | 2021/08/10 | Duel   | Prima versione                      |
 */
+import MIPBanner from '../components/page/MIPBanner'
 import MIPPage from '../components/page/MIPPage'
 import MIPPath from '../components/path/MIPPath'
 
@@ -16,7 +17,15 @@ export default function Layout({ eventData }) {
   return (
     <MIPPage className="mip-home-page1"
       pageTitle="Layout test">
+        <main className="">
+        <MIPBanner.CTABanner title="Nuovo bando mobilità sostenibile"
+          linkTitle="scopri ora come partecipare"
+          imageUrl="/news-images/bid-banner.png"/>
         <MIPPath.Controller className="mip-rounded-corners" responsive title="Calcola il percorso"/>
+        <MIPBanner.EmergencyBanner title="EMERGENZA COVID"
+          subtitle="Esenzione pagamento pedaggio per personale sanitario e associazioni di volontariato"
+          iconUrl="/icons/ambulance.svg" linkUrl="#" />
+        </main>
     </MIPPage>
   )
 }
