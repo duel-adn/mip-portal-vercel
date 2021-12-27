78769c012e14cbaae5cd00e42f18bf25528f1dda
dinotoa <sparetimebits@users.noreply.github.com>
2021-12-27 14:53:50 +0100

    Cambio ordinamento passi

--- components/pass/MIPPassAPI.js
+++ components/pass/MIPPassAPI.js
@@ -18,13 +18,16 @@ export async function fetchPassData({locale}) {
     const res = await fetch(process.env.MIP_PASS_DATA_URL)
     const rawPassData = await res.json()
     console.log(process.env.MIP_PASS_DATA_URL + ' ' + rawPassData.length)
+    const regex = /[A-Z](?=[^A-Z]*$)/
     const passData = rawPassData.map(rawPass => {
+        const capIndex = rawPass.Nome.match(regex).index
+        const sortName = rawPass.Nome.substring(capIndex)
         return {
             name: rawPass.Nome,
+            sortName: sortName,
             state: rawPass.Stato == 'Aperto',
             latitude: extractCoords(rawPass.Coordinate)[0],
             longitude: extractCoords(rawPass.Coordinate)[1]
         }})
-    
-    return passData.sort((p1, p2) => p1.name.localeCompare(p2.name))
-}
+        return passData.sort((p1, p2) => p1.sortName.localeCompare(p2.sortName))
+    }
